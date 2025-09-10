9170368b30d111f7fd1c07c80d455351eaab4869
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-09-10 18:22:16 +0200

    update example

--- examples/button.rs
+++ examples/button.rs
@@ -3,8 +3,8 @@ use wtk::prelude::*;
 
 fn main() {
     let mut app = App::<SDLBackend>::new("wtk example");
-    let button = Button::new("click", |b| {
-        b.set_text("newtext");
+    let button = Button::new("clickme", |b| {
+        b.set_text("clicked");
     }).shared();
     app.add_widget(button.clone());
     app.run();
